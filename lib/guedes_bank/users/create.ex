23eaa3f187511ddd76de66bdf27c926253a97a44
bruguedes <bruguedes@gmail.com>
2024-01-10 23:00:56 -0500

    fix: spec

--- lib/guedes_bank/users/create.ex
+++ lib/guedes_bank/users/create.ex
@@ -6,7 +6,7 @@ defmodule GuedesBank.Users.Create do
   alias GuedesBank.Users.Inputs.CreateUser
   alias GuedesBank.Users.Schema.User
 
-  @spec call(CreateUser.t()) :: {:ok, User.t()} | {:error, Ecto.Changeset.t()}
+  @spec call(CreateUser.t()) :: {:ok, User.t()} | {:errsor, atom() | Ecto.Changeset.t()}
   def call(%CreateUser{cep: cep} = params) do
     cep
     |> client().call()
